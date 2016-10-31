f1a6161dcb5a99fb2d31707c1543a4f55d94d506
Xingchen Hong <hello@xc-h.net>
2016-10-31 15:40:33 -0500

    Add command `meteord log`.

---
+++ src/commands/log.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const path = require('path');
+
+/**
+ * Gets the logs of the app container.
+ * @param {object} yargs -
+ * @param {CommandRoutineRnvironment} env -
+ * @return {void}
+ */
+module.exports = (yargs, env) => {
+
+  env.checkDeployFiles();
+
+  // Read config file.
+  const config = env.loadConfiguration(env.CONFIG_FILE_PATH);
+  const appName = config.appName,
+        appContainerName = env.getContainerName(appName);
+
+  const localLogFile = path.join(env.DEPLOY_DIR_PATH, 'container.log');
+
+  env.log(env.LOG_DIVIDER_STRING, localLogFile);
+  env.log(`Reading logs from app container '${appContainerName}'...`, localLogFile);
+
+  const result = env.remoteExecSync(config.server, [
+    `docker logs ${env.escq(appContainerName)}`
+  ], {
+    cwd: env.APP_ROOT_PATH,
+    log: localLogFile
+  });
+  env.checkStatus(result);
+  env.log(result.stdout);
+
+};
